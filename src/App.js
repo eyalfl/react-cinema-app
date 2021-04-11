46de62b0cedfc1805f9dfdd468eb36777bb8f28e
Eyal Fliess <eyalfl@yahoo.com>
2021-04-11 23:33:36 +0300

    add the Header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Provider } from 'react-redux';
 
 import store from './redux/store';
 import './App.scss';
+import Header from './components/header/Header';
 
 const App = () => {
   return (
     <Provider store={store}>
+      <Header />
       <div className="app">
         <h1>Redux initial setup</h1>
       </div>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import './Header.scss';
+import logo from '../../assets/cinema-logo.svg';
+
+const Header = () => {
+  // const [navClass, setNavClass] = useState(false);
+  // const [menuClass, setMenuClass] = useState(false);
+  const [toggleMenuState, setToggleMenuState] = useState(false);
+
+  const HEADER_LIST = [
+    {
+      id: 1,
+      iconClass: 'fas fa-film',
+      name: 'Now Playing',
+      type: 'now_playing'
+    },
+    {
+      id: 2,
+      iconClass: 'fas fa-fire',
+      name: 'Poplar',
+      type: 'poplar'
+    },
+    {
+      id: 3,
+      iconClass: 'fas fa-star',
+      name: 'Top Rated',
+      type: 'top_rated'
+    },
+    {
+      id: 4,
+      iconClass: 'fas fa-plus-square',
+      name: 'Upcoming',
+      type: 'upcoming'
+    }
+  ];
+
+  const toggleMenu = () => {
+    // setMenuClass(!menuClass);
+    // setNavClass(!menuClass);
+    setToggleMenuState(!toggleMenuState);
+    /* if (toggleMenuState) {
+      document.body.classList.add('header-nav-open');
+    } else {
+      document.body.classList.remove('header-nav-open');
+    } */
+  };
+
+  return (
+    <>
+      <div className="header-nav-container">
+        <div className="header-bar"></div>
+        <div className="header-navbar">
+          <div className="header-image">
+            <img src={logo} alt="" />
+          </div>
+          <div className="header-menu">
+            <div className={`${toggleMenuState ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`} id="header-mobile-menu" onClick={() => toggleMenu()}>
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </div>
+            <ul className={`${toggleMenuState ? 'header-nav header-mobile-nav' : 'header-nav'}`}>
+              {HEADER_LIST.map((data) => (
+                <li key={data.id} className="header-nav-item">
+                  <span className='header-list-name'>
+                    <i className={data.iconClass}></i>
+                  </span>
+                  &nbsp;
+                  <span className='header-list-name'>{data.name}</span>
+                </li>
+              ))}
+              <input className="search-input" type="text" placeholder="Search for a Movie"></input>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
